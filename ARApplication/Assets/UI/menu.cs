f4faf8a51750645a937a05f1a23f0e7c0bcd2375
Irreversible87 <49276631+Irreversible87@users.noreply.github.com>
2020-04-22 17:07:10 +0200

    User Interface
    ++ added basic ui design
    ++ startet scripting function for menu

---
+++ ARApplication/Assets/UI/menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    private Animator _animator;
+    private CanvasGroup _canvasGroup;
+
+    public bool isOpen
+    {
+        get 
+        {
+        return _animator.GetBool("isOpen"); 
+        }
+
+        set 
+        {
+            _animator.SetBool("isOpen", value);
+        }
+    }
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+
+        // change position of the menu at the start of the game to 0,0
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+    }
+
+    public void Update()
+    {
+        // check if the animator is in state "open". If not, block raycast and interaction. Else enable.
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+        }
+        else
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+        }
+    }
+}
---
+++ ARApplication/Assets/UI/menuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuManager : MonoBehaviour
+{
+    public menu currentMenu;
+
+    public void Start()
+    {
+        ShowMenu(currentMenu);
+    }
+
+    public void ShowMenu(menu menu) 
+    {
+        if (currentMenu != null)
+            currentMenu.isOpen = false;
+
+        currentMenu = menu;
+        currentMenu.isOpen = true;
+    }
+}
