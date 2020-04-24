2ddbddab321e2786ae6b148dda2ab3926787a4aa
Irreversible87 <49276631+Irreversible87@users.noreply.github.com>
2020-04-24 14:01:06 +0200

    Main Menu 1
    ++ added basic main menu functions
    ++ started creating pause menu

---
+++ ARApplication/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+
+            }
+        }
+    }
+}
